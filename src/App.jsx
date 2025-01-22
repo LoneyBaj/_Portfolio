11f60978a4130c493c845545f7c707a1efe3fc30
Loney Baj <lonitv.b14@gmail.com>
2025-01-22 15:52:52 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage.jsx'
 import POS from './Pages/POS.jsx'
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/pos" element={<POS />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/pos" element={<POS />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
